65bb818479c800ac98473e89fa9f77530a9aff39
Hellolife2750 <hellolife2750.programmation@gmail.com>
2023-10-19 23:23:04 +0200

    ajout de la page html permettant de filtrer les audios

---
+++ Audios-juil-22/AAAgetNames.ps1
@@ -0,0 +1,15 @@
+# Chemin du dossier courant
+$folderPath = Get-Location
+
+# Chemin du fichier de sortie
+$outputFile = Join-Path $folderPath "audiosNames.txt"
+
+# Liste tous les fichiers .ogg dans le dossier courant et les formate comme un tableau JavaScript
+$audioFiles = Get-ChildItem -Path $folderPath -Filter "*.ogg" | ForEach-Object { "`"$($_.Name)`"," }
+
+# Écrit les noms des fichiers dans le fichier de sortie avec le préfixe et le suffixe du tableau JavaScript
+$audioFiles = "const audioFiles = [" + ($audioFiles -join '') + "];"
+$audioFiles | Out-File -FilePath $outputFile -Encoding utf8
+
+Write-Host "Fichiers audio listés dans audiosNames.txt sous forme de tableau JavaScript :"
+Get-Content -Path $outputFile
