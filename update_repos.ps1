41f992f371b65eea3d39752c1ff2b51194ab9221
Hellolife2750 <hellolife2750.programmation@gmail.com>
2023-11-29 21:38:50 +0100

    ajout audios

---
+++ update_repos.ps1
@@ -0,0 +1,14 @@
+# Ajoute tous les fichiers au staging
+git add .
+
+git status
+
+Read-Host -Prompt "Entrée pour continuer"
+
+# Commit avec le message spécifié
+git commit -m "ajout audios"
+
+# Pousse les modifications vers le repo distant
+git push
+
+
